1b06ebf651fb48077114eb7a78dc66ec2b664cb9
VladOS-Developer <135268924+VladOS-Developer@users.noreply.github.com>
2025-08-01 16:33:26 +0300

    Add model file PasscodeState

---
+++ MovieApp/Module/PasscodeModule/Model/PasscodeState.swift
@@ -0,0 +1,16 @@
+//
+//  PasscodeState.swift
+//  MovieApp
+//
+//  Created by VladOS on 01.08.2025.
+//
+
+import UIKit
+
+enum PasscodeState {
+    case inputPasscode
+    case wrongPasscode
+    case setNewPasscode
+    case repeatPasscode
+    case codeMismatch
+}
