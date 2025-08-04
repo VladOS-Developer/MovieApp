b7b70bea750184a31be328d33f87b1b3c25d6c1a
VladOS-Developer <135268924+VladOS-Developer@users.noreply.github.com>
2025-08-04 10:53:46 +0300

    Add file KeychainManager + func:load, save, delete

---
+++ MovieApp/Module/PasscodeModule/Model/KeychainManager.swift
@@ -0,0 +1,47 @@
+//
+//  KeychainManager.swift
+//  MovieApp
+//
+//  Created by VladOS on 04.08.2025.
+//
+
+import Foundation
+import KeychainAccess
+
+enum KeychainKeys: String {
+    case passcode = "passcode1"
+}
+
+protocol KeychainManagerProtocol: AnyObject {
+    func save(key: String, value: String)
+    func load(key: String) -> Result <String , Error>
+}
+
+class KeychainManager: KeychainManagerProtocol {
+    
+    private let keychain = Keychain(service: "PASSCODE")
+    
+    func save(key: String, value: String) {
+        keychain[key] = value
+    }
+    
+    func load(key: String) -> Result <String, Error> {
+        do {
+            let passcode = try keychain.getString(key) ?? ""
+            return .success(passcode)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func delete(key: String) -> Result <Void, Error> {
+        do {
+            try keychain.remove(key)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    
+}
