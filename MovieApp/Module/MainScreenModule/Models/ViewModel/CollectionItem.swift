2e6c3557a913a622270361974a6585a50cda76ef
VladOS-Developer <135268924+VladOS-Developer@users.noreply.github.com>
2025-08-08 16:11:25 +0300

    Add folders and files to models / DomainModel, NetworkModel, ViewModel

---
+++ MovieApp/Module/MainScreenModule/Models/ViewModel/CollectionItem.swift
@@ -0,0 +1,8 @@
+//
+//  CollectionItem.swift
+//  MovieApp
+//
+//  Created by VladOS on 08.08.2025.
+//
+
+import Foundation
---
+++ MovieApp/Module/MainScreenModule/Models/ViewModel/MovieCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieCellViewModel.swift
+//  MovieApp
+//
+//  Created by VladOS on 08.08.2025.
+//
+
+import Foundation
